7fb443b782abc9fda9dea80b51ce10b15ce72517
Arthur Possmoser <arthurPossmoser.TI2024@gmail.com>
2025-04-25 12:29:57 -0300

    import

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import './index.css';               // seus estilos base
+import './App.css';                 // estilos globais do App
+import './assets/css/reset.css';    // reset de CSS
+import './assets/css/blog.css';     // estilos do Blog
+
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
